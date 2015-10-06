0ee48c2512d6245b69e3cacd45510443e60a2eb2
schnapple <plagam95@gmail.com>
2015-10-06 15:22:39 -0400

    Added the feature to retrieve all current processes running on the console. Added StdAfx.h for windows functions.

--- Windows/splitScreen.cpp
+++ Windows/splitScreen.cpp
@@ -1,9 +1,52 @@
+#include "StdAfx.h"
 #include <stdio.h>
 #include <string>
 #include <iostream>
 #include <fstream>
+#define _WIN32_WINNT 0x0501
+#include <windows.h>
+#include <tlhelp32.h>
+#include <tchar.h>
 
 
-int main(){
-	GetConsoleTitle();
+using namespace std;
+
+
+BOOL GetProcessList(){
+	HANDLE hProcessSnap;
+	HANDLE hProcess;
+	PROCESSENTRY32 pe32;
+	DWORD dwPriotiryClass;
+
+	// Take a snapshot of all processes in the systems
+	hProcessSnap = CreateToolhelp32Snapshot( TH32CS_SNAPPROCESS, 0);
+	if( hProcessSnap == INVALID_HANDLE_VALUE){
+		//printError( TEXT("CreateToolhelp32Snapshot (of processes)"));
+		return( FALSE );
+	}
+
+	pe32.dwSize = sizeof(PROCESSENTRY32);
+
+	if(!Process32First(hProcessSnap, &pe32)){
+		//printError( TEXT("Process32First"));
+		CloseHandle(hProcessSnap);
+		return( FALSE);
+	}
+
+	cout << "True" << endl;
+
+	do{
+		printf("PROCESS NAME: %s\n", pe32.szExeFile);
+	}while( Process32Next(hProcessSnap, &pe32));
+
+}
+
+
+int main(int argc, CHAR* argv[]){
+	//DWORD procIDs[6];
+	//DWORD maxCount = 6;
+	BOOL result = GetProcessList();
+	//cout << result << endl;
+	//cout << procIDs[0] << endl;
+	//cout << procIDs[1] << endl;
 }
